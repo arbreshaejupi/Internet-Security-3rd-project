013ed4e69bd006faa42dda7d6ffbc19b0aaa596d
ArbreshaHodolli <94081430+ArbreshaHodolli@users.noreply.github.com>
2023-01-08 13:29:53 +0100

    Add files via upload

---
+++ elftools/common/exceptions.py
@@ -0,0 +1,14 @@
+
+    pass
+
+class ELFRelocationError(ELFError):
+    pass
+
+class ELFParseError(ELFError):
+    pass
+
+class ELFCompressionError(ELFError):
+    pass
+
+class DWARFError(Exception):
+    pass
